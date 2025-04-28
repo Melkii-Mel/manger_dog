e71df5625da21b37f375443215a2ee4f5f5ddfe6
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2025-04-28 21:48:26 +0500

    refactor: get_path() and use_path_state()

--- web_client/src/base_hooks/use_path_state.rs
+++ web_client/src/base_hooks/use_path_state.rs
@@ -1,34 +1,41 @@
 use crate::navigation::URL_CHANGED;
 use crate::utils::get_path;
 use wasm_bindgen::closure::WasmClosureFnOnce;
+use wasm_bindgen::convert::ReturnWasmAbi;
 use web_sys::window;
-use yew::{hook, use_effect, use_state, UseStateHandle};
+use yew::{hook, use_effect, use_state, AttrValue, UseStateHandle};
 
 #[hook]
-pub fn use_path_state() -> UseStateHandle<String> {
+pub fn use_path_state() -> UseStateHandle<AttrValue> {
     let path = use_state(|| get_path());
     {
         let path = path.clone();
-        use_effect(move || {
-            let listener = js_sys::Function::from(
-                (move || {
-                    path.set(get_path());
-                })
-                .into_js_function(),
-            );
-            window()
-                .unwrap()
-                .add_event_listener_with_callback(URL_CHANGED, &listener)
-                .unwrap();
-
-            || {
-                window()
-                    .unwrap()
-                    .remove_event_listener_with_callback(URL_CHANGED, &listener)
-                    .unwrap();
-                drop(listener)
-            }
-        });
+        use_url_changed_event_listener(move || {
+            path.set(get_path());
+        })
     }
     path
+}
+
+#[hook]
+pub fn use_url_changed_event_listener<T, R>(callback: T)
+where
+    T: 'static + FnOnce() -> R,
+    R: ReturnWasmAbi + 'static,
+{
+    use_effect(move || {
+        let listener = js_sys::Function::from(callback.into_js_function());
+        window()
+            .unwrap()
+            .add_event_listener_with_callback(URL_CHANGED, &listener)
+            .unwrap();
+
+        || {
+            window()
+                .unwrap()
+                .remove_event_listener_with_callback(URL_CHANGED, &listener)
+                .unwrap();
+            drop(listener)
+        }
+    });
 }
--- web_client/src/utils.rs
+++ web_client/src/utils.rs
@@ -1,15 +1,17 @@
+use crate::config::get_config;
 use wasm_bindgen::JsValue;
 use web_sys::window;
-use crate::config::get_config;
+use yew::AttrValue;
 
-pub fn get_path() -> String {
-    window()
+pub fn get_path() -> AttrValue {
+    let path = window()
         .expect("failed to get window while trying to get path")
         .location()
         .pathname()
         .expect("failed to get location pathname while trying to get path")
         [get_config().base_url.len()..]
-        .to_string()
+        .to_string();
+    AttrValue::from(path)
 }
 
 pub fn log(str: &str) {
