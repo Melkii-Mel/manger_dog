08244ad5a6a6060ac01e07c65edde20c815aeb24
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2025-05-14 20:24:30 +0500

    fix(entities): remove UserId, replace RecordId with RecordOf<T>

--- server/entities/src/lib.rs
+++ server/entities/src/lib.rs
@@ -33,7 +33,6 @@ api_entities!(
     // TODO: Validate other fields like currency_id. It appears that by default surrealDB allows adding non-existing ids freely
     Account|AccountError("accounts" ["user_id"]) {
         title: String [not_empty],
-        user_id: RecordId,
         currency_id: RecordId,
         balance: i64,
     }
@@ -50,75 +49,70 @@ api_entities!(
     }
 
     Tag|TagError("tags" ["user_id", "metadata_id.user_id"]) {
-        user_id: RecordId,
         metadata_id: RecordOf<Metadata>|MetadataError,
     }
 
     MetadataTag|MetadataTagError("metadata_tags" ["metadata_id.user_id"]) {
-        metadata_id: RecordId,
-        tag_id: RecordId,
+        metadata_id: RecordOf<Metadata>,
+        tag_id: RecordOf<Tag>,
         exception: bool,
     }
 
     // TODO: make paths work properly with Options
     TagGroup|TagGroupError("tag_groups" ["user_id", "metadata_id.user_id"]) {
-        user_id: RecordId,
-        metadata_id: Option<RecordId>,
+        metadata_id: Option<RecordOf<Metadata>>,
     }
 
     TagGroupTag|TagGroupTagError("tag_group_tags" ["tag_group_id.user_id", "tag_id.user_id"]) {
-        tag_group_id: RecordId,
-        tag_id: RecordId,
+        tag_group_id: RecordOf<TagGroup>,
+        tag_id: RecordOf<Tag>,
     }
 
     MetadataTagGroup|MetadataTagGroupError("metadata_tag_groups" ["metadata_id.user_id", "tag_group_id.user_id"]) {
-        metadata_id: RecordId,
-        tag_group_id: RecordId,
+        metadata_id: RecordOf<Metadata>,
+        tag_group_id: RecordOf<TagGroup>,
     }
 
     FinancialGoal|FinancialGoalError("financial_goals" ["user_id", "metadata_id.user_id"]) {
-        user_id: RecordId,
         currency_id: RecordId,
         start_date: DateTime<Utc> [v1_gt_v2(end_date)],
         end_date: DateTime<Utc>,
         target_income: i64 [gt_zero],
-        metadata_id: RecordId,
+        metadata_id: RecordOf<Metadata>,
     }
 
     Transaction|TransactionError("transactions" ["account_id.user_id", "metadata_id.user_id"]) {
-        account_id: RecordId,
+        account_id: RecordOf<Account>,
         amount: i64,
         date: DateTime<Utc>,
-        metadata_id: RecordId,
+        metadata_id: RecordOf<Metadata>,
     }
 
     Transfer|TransferError("transfers" ["account_from.user_id", "metadata_id.user_id"]) {
-        account_from: String,
-        account_to: String,
+        account_from: RecordOf<Account>,
+        account_to: RecordOf<Account>,
         amount_from: i64 [gt_zero],
         amount_to: i64 [gt_zero],
         conversion_rate: f64 [gt_zero],
         fee: f64 [gt_zero],
-        metadata_id: RecordId,
+        metadata_id: RecordOf<Metadata>,
     }
 
     StableIncome|StableIncomeError("stable_incomes" ["user_id", "metadata_id.user_id"]) {
-        user_id: RecordId,
         currency_id: RecordId,
         amount_per_month: i64 [ne_zero],
         start_date: DateTime<Utc> [optional_v2_gt_v1(end_date)],
         end_date: Option<DateTime<Utc>>,
         last_update_date: DateTime<Utc>,
-        metadata_id: RecordId,
+        metadata_id: RecordOf<Metadata>,
     }
 
     StableIncomeIncome|StableIncomeIncomeError("stable_income_incomes" ["stable_income_id.user_id", "transaction_id.user_id"]) {
-        stable_income_id: RecordId,
-        transaction_id: RecordId,
+        stable_income_id: RecordOf<StableIncome>,
+        transaction_id: RecordOf<Transaction>,
     }
 
     Loan|LoanError("loans" ["user_id", "metadata_id.user_id"]) {
-        user_id: RecordId,
         currency_id: RecordId,
         principal_amount: i64 [gt_zero],
         interest_rate: f64 [gt_zero],
@@ -126,16 +120,15 @@ api_entities!(
         end_date: Option<DateTime<Utc>>,
         interest_rate_type: String,
         compounding_frequency: String,
-        metadata_id: RecordId,
+        metadata_id: RecordOf<Metadata>,
     }
 
     LoanPayment|LoanPaymentError("loan_payments" ["loan_id.user_id", "transaction_id.user_id"]) {
-        loan_id: RecordId,
-        transaction_id: RecordId,
+        loan_id: RecordOf<Loan>,
+        transaction_id: RecordOf<Transaction>,
     }
 
     Investment|InvestmentError("investments" ["user_id", "metadata_id.user_id"]) {
-        user_id: RecordId,
         currency_id: RecordId,
         r#type: String,
         compounding_frequency: String,
@@ -145,69 +138,67 @@ api_entities!(
         end_date: Option<DateTime<Utc>>,
         risk_level: String,
         expected_return: f64 [gt_zero],
-        metadata_id: RecordId,
+        metadata_id: RecordOf<Metadata>,
     }
 
     InvestmentReturn|InvestmentReturnError("investment_returns" ["investment_id.user_id", "transaction_id.user_id"]) {
-        investment_id: RecordId,
-        transaction_id: RecordId,
+        investment_id: RecordOf<Investment>,
+        transaction_id: RecordOf<Transaction>,
     }
 
     Metadata|MetadataError("metadata" ["user_id"]) {
         title: Option<String>,
         description: Option<String>,
-        user_id: RecordId,
     }
 
     Preference|PreferenceError("preferences" ["user_id"]) {
-        user_id: RecordId,
         default_currency_id: RecordId,
         language: String,
     }
 
-    AutoDistribution|AutoDistributionError("auto_distributions" ["account_id.user_id", "metadata_id.user_id"]) {
+    AutoDistribution|AutoDistributionError("auto_distributions" ["account_id.user_id", "metadata_id.user_id", "record_metadata_id.user_id"]) {
         ratio: f64 [gt_zero],
-        account_id: RecordId,
-        metadata_id: RecordId,
-        record_metadata_id: RecordId,
+        account_id: RecordOf<Account>,
+        metadata_id: RecordOf<Metadata>,
+        record_metadata_id: RecordOf<Metadata>,
     }
 
     FinancialGoalAutoDistribution|FinancialGoalAutoDistributionError("financial_goal_auto_distributions" ["financial_goal_id.user_id", "auto_distribution_id.account_id.user_id"]) {
-        financial_goal_id: RecordId,
-        auto_distribution_id: RecordId,
+        financial_goal_id: RecordOf<FinancialGoal>,
+        auto_distribution_id: RecordOf<AutoDistribution>,
     }
 
     StableIncomeAutoDistribution|StableIncomeAutoDistributionError("stable_income_auto_distributions" ["stable_income_id.user_id", "auto_distribution_id.account_id.user_id"]) {
-        stable_income_id: RecordId,
-        auto_distribution_id: RecordId,
+        stable_income_id: RecordOf<StableIncome>,
+        auto_distribution_id: RecordOf<AutoDistribution>,
     }
 
     LoanAutoDistribution|LoanAutoDistributionError("loan_auto_distributions" ["loan_id.user_id", "auto_distribution_id.account_id.user_id"]) {
-        loan_id: RecordId,
-        auto_distribution_id: RecordId,
+        loan_id: RecordOf<Loan>,
+        auto_distribution_id: RecordOf<AutoDistribution>,
     }
 
     InvestmentAutoDistribution|InvestmentAutoDistributionError("investment_auto_distributions" ["investment_id.user_id", "auto_distribution_id.account_id.user_id"]) {
-        investment_id: RecordId,
-        auto_distribution_id: RecordId,
+        investment_id: RecordOf<Investment>,
+        auto_distribution_id: RecordOf<AutoDistribution>,
     }
 
     TransferAutoDistribution|TransferAutoDistributionError("transfer_auto_distributions" ["auto_distribution_id.account_id.user_id", "metadata_id.user_id"]) {
-        metadata_id: RecordId,
-        account_to: String,
-        auto_distribution_id: RecordId,
+        metadata_id: RecordOf<Metadata>,
+        account_to: RecordOf<Account>,
+        auto_distribution_id: RecordOf<AutoDistribution>,
     }
 
     TransactionAutoDistribution|TransactionAutoDistributionError("transaction_auto_distributions" ["auto_distribution_id.account_id.user_id"]) {
-        auto_distribution_id: RecordId,
+        auto_distribution_id: RecordOf<AutoDistribution>,
     }
 
-    FinancialGoalAllocations|FinancialGoalAllocationsError("financial_goal_allocations") {
-        financial_goal_id: RecordId,
-        account_id: Option<String>,
+    FinancialGoalAllocations|FinancialGoalAllocationsError("financial_goal_allocations" ["financial_goal_id.user_id", "account_id.user_id", "metadata_id.user_id"]) {
+        financial_goal_id: RecordOf<FinancialGoal>,
+        account_id: Option<RecordOf<Account>>,
         date: DateTime<Utc>,
         amount: i64 [gt_zero],
-        metadata_id: RecordId,
+        metadata_id: RecordOf<Metadata>,
     }
 );
 
