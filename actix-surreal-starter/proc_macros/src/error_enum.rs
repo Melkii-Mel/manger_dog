a015abe43a5f6edc9b391e281a211a7ab7a6581a
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2025-05-24 12:35:22 +0500

    add ErrorEnum derive macro
    allows to get string representation of error enums for localization purposes

---
+++ actix-surreal-starter/proc_macros/src/error_enum.rs
@@ -0,0 +1,43 @@
+use quote::quote;
+use proc_macro::{TokenStream};
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
+
+pub fn error_enum(attr: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(attr as DeriveInput);
+    let enum_ident = input.ident;
+    let variants = match input.data {
+        Data::Enum(data) => data.variants,
+        _ => panic!("Only works with enums")
+    };
+    let arms = variants.iter().map(|variant| {
+        let arm_ident = &variant.ident;
+        let arm_name = arm_ident.to_string();
+        match &variant.fields {
+            Fields::Unnamed(fields) if fields.unnamed.len() == 1 => {
+                quote! {
+                    #enum_ident::#arm_ident(v) => format!("{}.{}", #arm_name, v.as_dot_path()),
+                }
+            }
+            Fields::Unit => {
+                quote! {
+                    #enum_ident::#arm_ident => #arm_name.to_string(),
+                }
+            }
+            _ => {
+                panic!("only unnamed field or unit arms are supported");
+            }
+        }
+    });
+    
+    let expanded = quote! {
+        impl ::actix_surreal_starter_types::ErrorEnum for #enum_ident {
+            fn as_dot_path(&self) -> String {
+                match self {
+                    #(#arms)*
+                }
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
